c75564bd98c7fcb2c58d75f85caa2bec98feb330
XochitlBelen <xochitlbelencruz01@gmail.com>
2023-06-18 15:56:14 -0600

    Initial commit

---
+++ src/main/java/com/example/ferreteriaa/FerreteriaaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ferreteriaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FerreteriaaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FerreteriaaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ferreteriaa/FerreteriaaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ferreteriaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FerreteriaaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
